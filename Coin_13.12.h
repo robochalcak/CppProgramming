784f75c9b4fcb687cf5296bd07a6546015acac29
RobertChalcak <50630008+RobertChalcak@users.noreply.github.com>
2019-05-18 14:57:38 +0200

    Create Coin_13.12.h
    ProgrammingChallenges13.12 Toin Toss simulator

---
+++ Coin_13.12.h
@@ -0,0 +1,39 @@
+#pragma once
+#ifndef COIN_H
+#define COIN_H
+#include<cmath>
+#include<iostream>
+#include<ctime>
+#include<string>
+using namespace std;
+
+class Coin
+{
+private:
+	string sideUp;
+public:
+	Coin()
+	{
+		int seed(time(0));
+		srand(seed);
+		if ((rand() % 2) == 0)
+			sideUp = "Heads";
+		else
+			sideUp = "Tails";
+	}
+	~Coin(){}
+	void toss()
+	{
+		if ((rand()%2)==0)
+			sideUp = "Heads";
+		else
+			sideUp = "Tails";
+	}
+
+	string getSideUp()const
+	{
+		return sideUp;
+	}
+};
+
+#endif // !COIN_H
